524d8d66f857a2592780f02291d8f281c63f382d
Gabriel Portela Gomes <gabrielgomesportela@gmail.com>
2023-07-11 08:04:31 -0300

    feat: global exception handler

---
+++ src/main/java/com/api/hiring/hiring/exception/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.api.hiring.hiring.exception;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        return new ResponseEntity<>("Nome inválido", HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<String> handleDataIntegrityViolationException(DataIntegrityViolationException e) {
+        return new ResponseEntity<>("Candidato já participa do processo", HttpStatus.FORBIDDEN);
+    }
+
+}
